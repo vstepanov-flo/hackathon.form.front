6de230e86435dd82700234982349147f3f8f7767
Vlad <vstepanov@flo.team>
2022-04-08 22:37:35 +0300

    feat: add api service

---
+++ src/app/shared/services/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  #get<T>(url:string): Observable<T> {
+    return this.httpClient.get<T>(url);
+  }
+
+  #post<T>(url:string, body: any): Observable<T> {
+    return this.httpClient.post<T>(url, body);
+  }
+}
