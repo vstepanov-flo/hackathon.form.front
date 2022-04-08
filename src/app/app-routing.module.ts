31c12c3902d05548d7501c34341aa78e4504028d
Vlad <vstepanov@flo.team>
2022-04-08 20:33:43 +0300

    feat: add router paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmailApprovalComponent } from './components/email-approval/email-approval.component';
+import { ReportFormComponent } from './components/report-form/report-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ReportFormComponent },
+  { path: 'email-verify', component: EmailApprovalComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
