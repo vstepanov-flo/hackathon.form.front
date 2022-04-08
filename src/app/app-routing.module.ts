1e778415076fd07ca6f8ce12864fa922cc3a2f3c
Vlad <vstepanov@flo.team>
2022-04-08 22:39:30 +0300

    feat(component): add success page component and page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmailApprovalComponent } from './components/email-approval/email-approval.component';
 import { ReportFormComponent } from './components/report-form/report-form.component';
+import { SuccessPageComponent } from './components/success-page/success-page.component';
 
 const routes: Routes = [
   { path: '', component: ReportFormComponent },
   { path: 'email-verify', component: EmailApprovalComponent },
+  { path: 'success', component: SuccessPageComponent },
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
---
+++ src/app/components/success-page/success-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-success-page',
+  templateUrl: './success-page.component.html',
+  styleUrls: ['./success-page.component.scss']
+})
+export class SuccessPageComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.router.navigate(['']).then();
+    }, 5000)
+  }
+
+}
