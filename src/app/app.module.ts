2353beecacda34bf39bdde233c9b707c5bed104e
Vlad <vstepanov@flo.team>
2022-04-08 20:33:28 +0300

    feat: add nebular modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbIconModule,
+  NbFormFieldModule,
+  NbInputModule,
+  NbCardModule, NbSelectModule, NbTagModule, NbAutocompleteModule, NbButtonModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ReportFormComponent } from './components/report-form/report-form.component';
+import { EmailApprovalComponent } from './components/email-approval/email-approval.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportFormComponent,
+    EmailApprovalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbIconModule,
+    NbFormFieldModule,
+    NbInputModule,
+    ReactiveFormsModule,
+    NbCardModule,
+    NbSelectModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbTagModule,
+    NbAutocompleteModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
