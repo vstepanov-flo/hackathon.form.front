a6f153d9f4b494912f37422ebe3686395bd658f3
Vlad <vstepanov@flo.team>
2022-04-09 01:51:39 +0300

    feat: add request for get client city

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export interface ObjectType {
+  [k:string]: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +14,14 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  #get<T>(url:string): Observable<T> {
-    return this.httpClient.get<T>(url);
+  public getCity(): Observable<any> {
+    return this.#get('https://api.ipgeolocation.io/ipgeo', { apiKey: environment.ipgeolocationApiKey });
+  }
+
+  #get<T>(url:string, params?: ObjectType): Observable<T> {
+    return this.httpClient.get<T>(url, {
+      params
+    });
   }
 
   #post<T>(url:string, body: any): Observable<T> {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ipgeolocationApiKey: '6afebe20a77848e996c1e31deac91dfe'
 };
 
 /*
